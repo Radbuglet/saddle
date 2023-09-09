9795082de98564401324a9cd06b301c6f626f6f8
Radbuglet <10578060+Radbuglet@users.noreply.github.com>
2023-09-09 08:41:48 -0600

    Implement `scope!` inheritance

--- src/saddle/src/lib.rs
+++ src/saddle/src/lib.rs
@@ -10,6 +10,24 @@ pub mod scope_macro_internals {
         partial_scope::partial_shadow,
     };
 
+    pub trait BindScopeAsRef: Scope {
+        fn __saddle_internal_bind_scope(&self) -> BoundScopeProof<'_, Self>;
+    }
+
+    impl<T: ?Sized + Scope> BindScopeAsRef for T {
+        fn __saddle_internal_bind_scope(&self) -> BoundScopeProof<'_, Self> {
+            BoundScopeProof(self)
+        }
+    }
+
+    pub struct BoundScopeProof<'a, T: ?Sized>(&'a T);
+
+    impl<'a, T: ?Sized> BoundScopeProof<'a, T> {
+        pub fn unwrap(self) -> &'a T {
+            self.0
+        }
+    }
+
     pub fn limit_lifetime<'a, T: ?Sized>(_limiter: &'a mut (), value: &'a T) -> &'a T {
         value
     }
@@ -20,19 +38,28 @@ pub mod scope_macro_internals {
 #[macro_export]
 macro_rules! scope {
     (
-        $from:expr => $to:ident;
+        $from:expr => $to:ident $(inherits $($grant_kw:ident $grant_ty:ty),*$(,)?)?;
         $($body:tt)*
     ) => {
 		let mut __lifetime_limiter = ();
         let __scope_internal_to_token = {
-            use $crate::scope_macro_internals::Scope as _;
+			let from = {
+				use $crate::scope_macro_internals::BindScopeAsRef as _;
+				$crate::scope_macro_internals::BoundScopeProof::unwrap($from.__saddle_internal_bind_scope())
+			};
+
+			$($($crate::scope_macro_internals::scope!(@__decl_dep from, $grant_kw $grant_ty);)*)?
 
             $crate::scope_macro_internals::scope!(InlineBlock);
 
-            $crate::scope_macro_internals::limit_lifetime::<InlineBlock>(
+            let to = $crate::scope_macro_internals::limit_lifetime::<InlineBlock>(
 				&mut __lifetime_limiter,
-				$from.decl_call::<InlineBlock>(),
-			)
+				$crate::scope_macro_internals::Scope::decl_call::<InlineBlock>(from),
+			);
+
+			$($($crate::scope_macro_internals::scope!(@__decl_grant to, $grant_kw $grant_ty);)*)?
+
+			to
         };
 
         $crate::scope_macro_internals::partial_shadow! {
@@ -43,11 +70,11 @@ macro_rules! scope {
 		$crate::scope_macro_internals::drop(__lifetime_limiter);
     };
     (
-        $from_and_to:ident:
+        $from_and_to:ident $(inherits $($grant_kw:ident $grant_ty:ty),*$(,)?)?:
         $($body:tt)*
     ) => {
         $crate::scope_macro_internals::scope! {
-            $from_and_to => $from_and_to;
+            $from_and_to => $from_and_to $(inherits $($grant_kw $grant_ty),*)?;
             $($body)*
         }
     };
@@ -73,6 +100,18 @@ macro_rules! scope {
             }
         }
     )*};
+	(@__decl_dep $target:expr, ref $ty:ty) => {
+		$crate::scope_macro_internals::Scope::decl_dep_ref::<$ty>($target);
+	};
+	(@__decl_dep $target:expr, mut $ty:ty) => {
+		$crate::scope_macro_internals::Scope::decl_dep_mut::<$ty>($target);
+	};
+	(@__decl_grant $target:expr, ref $ty:ty) => {
+		$crate::scope_macro_internals::Scope::decl_grant_ref::<$ty>($target);
+	};
+	(@__decl_grant $target:expr, mut $ty:ty) => {
+		$crate::scope_macro_internals::Scope::decl_grant_mut::<$ty>($target);
+	};
 }
 
 pub trait Scope: 'static + Sized {
